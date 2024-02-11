f74825deef5577060cbd0077fc210d310f91fb41
dhatrikollisetty <dhatrikollisetty@gmail.com>
2024-02-11 11:09:51 +0530

    student

---
+++ src/Student.java
@@ -0,0 +1,9 @@
+public class Student{
+    String name;
+    String fatherName;
+
+    public Student(String name, String fatherName) {
+        this.name = name;
+        this.fatherName = fatherName;
+    }
+}
